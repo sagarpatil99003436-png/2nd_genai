718c89ab0a4efa085fed642d1d8a8a29412afcff
Mallanagouda s patil <sagarpatil99003436@gmail.com>
2026-09-14 12:11:42 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+
+load_dotenv()
+
+loader = PyPDFLoader(
+    r"D:\\DSProject\\2ndGen_AI\\document_loader\\book deeplearning.pdf"
+)
+
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+chunks = splitter.split_documents(docs)
+
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vectorstore = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    persist_directory="chroma_db"
+)
+
+print("Embeddings stored successfully.")
---
+++ db.py
@@ -0,0 +1,47 @@
+from dotenv import load_dotenv
+from langchain_core.documents import Document
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+docs = [
+    Document(
+        page_content="Python is widely used in Artificial Intelligence.",
+        metadata={"source": "AI_book"}
+    ),
+    Document(
+        page_content="Pandas is used for data analysis in Python.",
+        metadata={"source": "DataScience_book"}
+    ),
+    Document(
+        page_content="Neural networks are used in deep learning.",
+        metadata={"source": "DL_book"}
+    ),
+]
+
+embedding_model = OpenAIEmbeddings()
+
+vectorstore = Chroma.from_documents(
+    documents=docs,
+    embedding=embedding_model,
+    persist_directory="chroma_db"
+)
+
+result = vectorstore.similarity_search(
+    "what is used for data analysis?",
+    k=2
+)
+
+for r in result:
+    print(r.page_content)
+    print(r.metadata)
+
+retriever = vectorstore.as_retriever()
+
+retrieved_docs = retriever.invoke(
+    "Explain deep learning"
+)
+
+for d in retrieved_docs:
+    print(d.page_content)
